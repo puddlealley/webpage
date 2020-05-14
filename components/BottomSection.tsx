d96857d46fab5a6723929dd5be68f36ae6853d7d
Robert Parkinson <robert.parkinson@hmcts.net>
2020-05-14 10:26:43 +0100

    Clean up bottom section

--- components/BottomSection.tsx
+++ components/BottomSection.tsx
@@ -1,40 +1,21 @@
 import { NextPage } from 'next';
+import * as React from 'react';
 import Link from 'next/link'
 
 const BottomSection: NextPage<{}> = () => {
     return (
-        <section>
-            <div className="container background pr-24 md:pl-24 py-10">
-                <h1 className= "text-4xl mb-4 text-right">How we work</h1>
-                <p className="mb-4 w-1/3 text-right">We're a small operation with a small number of clients and projects. We hope to grow and have a large network of contractors and friends that we use to take on larger projects.</p>
-                <Link href={"/about"}>
-                    <button className="py-3 px-5 rounded-full font-bold">Work with us</button>
-                </Link>
-                <div className="clearBoth"></div>
-            </div>
+        <section className="flex-row background bottom-section">
+            <h1 className= "text-4xl mb-4 text-right">How we work</h1>
+            <p className="mb-4 w-1/3 text-right ml-auto">We're a small operation with a small number of clients and projects. We hope to grow and have a large network of contractors and friends that we use to take on larger projects.</p>
+            <Link href={"/about"}>
+                <button className="py-3 px-5 rounded-full font-bold ml-auto block">Work with us</button>
+            </Link>
 
             <style jsx>{`
-                section {
-                    background-color: #03325D;
-                    color: #F6FBF5;
-                    width: 100%;
-                }
                 .background {
                     background-image: url(/images/puddle.png);
                     background-position: center left;
                     background-repeat: no-repeat;
-                }
-                p {
-                    float: right;
-                }
-                button {
-                    clear: both;
-                    float: right;
-                    background-color: #FC993E;
-                    color: #103650;
-                }
-                .clearBoth {
-                    clear:both;
                 }
             `}
             </style>
