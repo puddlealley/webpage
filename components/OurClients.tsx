6cf659f62ca11eac9a86b207b8bbf02161a996cf
Robert Parkinson <robert.parkinson@hmcts.net>
2020-05-14 10:35:59 +0100

    Cleanup our clients section

--- components/OurClients.tsx
+++ components/OurClients.tsx
@@ -5,11 +5,11 @@ const OurClients: NextPage<{}> = () => {
         <section className="flex-row pl-24 md:pr-24 py-10">
             <h1 className="text-4xl font-bold text-center">Our clients</h1>
             <p className="text-center">If you're curious about what we do, here are a few logos of some of the clients and projects I have worked on in the past.</p>
-            <div className="flex my-10 justify-center">
-                <img className="mr-10" src="/images/ustwo.png" alt="UsTwo logo"/>
-                <img className="mr-10" src="/images/sky.png" alt="Sky logo"/>
-                <img className="mr-10" src="/images/cirruslogic.png" alt="Cirrus Logic logo"/>
-                <img className="mr-10" src="/images/zipcar.png" alt="Zipcar logo"/>
+            <div className="flex my-10 justify-between">
+                <img src="/images/ustwo.png" alt="UsTwo logo"/>
+                <img src="/images/sky.png" alt="Sky logo"/>
+                <img src="/images/cirruslogic.png" alt="Cirrus Logic logo"/>
+                <img src="/images/zipcar.png" alt="Zipcar logo"/>
                 <img src="/images/carv.png" alt="Carv logo"/>
             </div>
             <p className="text-center">I've helped companies develop and test next-gen digital signal processors and built software to help vehicle manufactures track Quality Assurance. I've also ran and maintained several social networks. (Popjam and Curated). </p>
@@ -17,7 +17,6 @@ const OurClients: NextPage<{}> = () => {
             <style jsx>{`
                 section {
                     color: #04335E;
-                    width: 100%;
                 }
                 img {
                     height: auto;
