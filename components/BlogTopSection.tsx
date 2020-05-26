88bdc90bebabf2c212eb88399135344fe8e50a2e
Robert Parkinson <robert.parkinson@hmcts.net>
2020-05-26 17:08:30 +0100

    Mobile version of top section of blog page

--- components/BlogTopSection.tsx
+++ components/BlogTopSection.tsx
@@ -7,18 +7,26 @@ type Props = {
 }
 
 const BlogTopSection: React.FunctionComponent<Props> = ({ post}) => (
-    <section className="flex-row background top-section md:pl-24 md:py-10">
-        <h1 className="text-4xl mb-4 w-1/2 font-bold">{ post.title }</h1>
-        <p className="mb-4 w-1/2">{ post.content }</p>
+    <section className="flex-row background top-section md:pl-24 md:py-10 pb-10">
+        <div className="md:hidden relative pb-2/3">
+            <img className="absolute top-0 h-full w-full object-cover" src="/images/notebook.png" alt="Notebook"/>
+        </div>
+        <h1 className="text-4xl md:w-1/2 p-6 mb-4 font-bold text-center md:text-left">{ post.title }</h1>
+        <p className="mb-4 px-6 md:w-1/2 text-center md:text-left text-justify">{ post.content }</p>
         <Link href={`/blog/${post.id}`}>
-            <button className="py-3 px-5 rounded-full font-bold">Read more</button>
+            <button className="py-3 ml-auto px-5 rounded-full font-bold mx-auto md:mx-6 block">Read more</button>
         </Link>
 
         <style jsx>{`
             .background {
-                background-image: url(/images/notebook.png);
-                background-position: center right;
-                background-repeat: no-repeat;
+                background-image: none;
+            }
+            @media (min-width: 640px) {
+                  .background {
+                    background-image: url(/images/notebook.png);
+                    background-position: center right;
+                    background-repeat: no-repeat;
+                }
             }
         `}
         </style>
