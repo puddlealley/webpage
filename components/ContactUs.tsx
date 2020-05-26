06bc68080f66e4b7b071b055f43cf668bb58ef4e
Robert Parkinson <robert.parkinson@hmcts.net>
2020-05-26 16:24:30 +0100

    mobile version of contact us

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -3,32 +3,29 @@ import React from 'react'
 
 const ContactUs: NextPage<{}> = () => {
     return (
-        <section id={"contact"} className="background pl-24 md:pr-24 py-10">
-            <h1 className="text-4xl font-bold mb-8">Get in touch</h1>
-            <div className="flex flex-col mb-16">
+        <section id="contact" className="background top-section md:pl-24 md:pr-24 py-10">
+            <div className="md:hidden relative pb-2/3">
+                <img className="absolute top-0 h-full w-full object-cover" src="/images/street-puddle.png" alt="Puddle reflect street above"/>
+            </div>
+            <h1 className="text-4xl px-6 mb-4 font-bold text-center md:text-left">Get in touch</h1>
+            <div className="flex-col px-6 mb-16">
                 <label htmlFor="name" className="block mb-2 font-bold">Your name</label>
-                <input id="name" type="text" className="block w-2/5 mb-10 rounded text-2xl"/>
+                <input id="name" type="text" className="block w-full md:w-2/5 mb-10 rounded text-2xl"/>
                 <label htmlFor="message" className="block mb-2 font-bold">Message</label>
-                <textarea name="message" id="message" className="block w-2/5 mb-10 h-64 rounded"></textarea>
-                <button className="py-3 px-5 rounded-full w-1/6 font-bold">Send message</button>
+                <textarea name="message" id="message" className="block w-full md:w-2/5 mb-10 h-64 rounded"></textarea>
+                <button className="py-3 px-5 rounded-full mx-auto md:mx-0 font-bold">Send message</button>
             </div>
 
             <style jsx>{`
-                section {
-                    background-color: #E7F9F9;
-                    color: #04335E;
-                    width: 100%;
-                }
                 .background {
-                    background-image: url(/images/street-puddle.png);
-                    background-position: center right;
-                    background-repeat: no-repeat;
+                    background-image: none;
                 }
-                button {
-                    clear: both;
-                    float: left;
-                    background-color: #FC993E;
-                    color: #103650;
+                @media (min-width: 640px) {
+                      .background {
+                        background-image: url(/images/street-puddle.png);
+                        background-position: center right;
+                        background-repeat: no-repeat;
+                    }
                 }
             `}
             </style>
